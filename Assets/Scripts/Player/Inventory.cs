42faf62bc085ae0c95c8905f3048cc70c255a231
Reinard Kurokitsukami <guitaristkun@gmail.com>
2018-10-25 17:27:41 +0300

    gitignore

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterInventory
+
+public class Inventory {
+
+}
+
+public class Item {
+	
+}
+
+public class InventoryItem {
+	Item item;
+	int amount;
+
+	public int StackSize { get { return 64; } }
+	public int Stack { get { return amount; } }
+
+	public InventoryItem (Item i) {
+		
+	}
+}
